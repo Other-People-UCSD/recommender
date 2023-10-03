2b58a75655eb174e0b8441324e29bbe40c7eff4f
Kevin-Jjang <k1jang@ucsd.edu>
2023-10-03 16:26:52 -0700

    Convert notebook to python script

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Convert the result to JSON and write to file
+with open('output.json', mode='w', encoding='utf8') as outfile:
+    outfile.write('{"out":"this is a test output"}')
+
+
+# In[ ]:
+
+
+
+
